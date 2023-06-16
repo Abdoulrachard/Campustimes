f9d415d493273115efbcee26541c7f2daafe332f
Abdoulrachard <Abdoulrachard@gmail.com>
2023-06-16 01:22:06 +0100

    Modification de certaine fichier

--- App/views.py
+++ App/views.py
@@ -6,3 +6,8 @@ def default(request):
     context = {}
     
     return render(request, 'app/default.html', context)
+def dashbord(request):
+    
+    context = {}
+    
+    return render(request, 'app/dashbord.html', context)
